91535fc5d97bd21cb6b19b3c9f47573daea0a59b
mqc <0x0muu@gmail.com>
2023-09-08 17:01:43 +0900

    add matplot

--- modules/AudioStream.py
+++ modules/AudioStream.py
@@ -9,6 +9,9 @@ class AudioStream:
         self.samplerate = samplerate
         self.phase = 0  # 追加: 継続的に更新されるフェーズ変数
 
+        # 最後に生成された音声データを保持するためのバッファ
+        self.last_data = np.zeros(int(self.samplerate * 0.1))  # 0.1秒分
+
         # ストリームを開始
         self.stream = sd.OutputStream(
             samplerate=self.samplerate, channels=1, callback=self.callback
@@ -21,15 +24,31 @@ class AudioStream:
         # outdata[:, 0] = self.amplitude * np.sin(2 * np.pi * self.frequency * t)
 
         t = np.arange(frames) / self.samplerate
+        # outdata[:, 0] = self.amplitude * np.sin(
+        #     2 * np.pi * self.frequency * t + self.phase
+        # )
+
+        # f0 = self.frequency
+        # outdata[:, 0] = 0.07 * np.sin(2 * np.pi * t * f0 * 1 + self.phase)
+        # +0.09 * np.sin(2 * np.pi * t * f0 * 2 + self.phase)
+        # +0.08 * np.sin(2 * np.pi * t * f0 * 3 + self.phase)
+        # +0.19 * np.sin(2 * np.pi * t * f0 * 4 + self.phase)
+        # +0.08 * np.sin(2 * np.pi * t * f0 * 5 + self.phase)
+        # +0.07 * np.sin(2 * np.pi * t * f0 * 6 + self.phase)
+
         outdata[:, 0] = self.amplitude * np.sin(
             2 * np.pi * self.frequency * t + self.phase
         )
+
         # プツプツというノイズは、サイン波の連続性が失われている場合や、バッファとの境界でのフェーズの不連続性に起因する可能性があります。
         # この問題を解消するためには、フェーズの連続性を保つための変数を導入して、コールバックが呼ばれるたびにフェーズを継続的に更新することが必要です。
 
         # 最後のフェーズを更新して次回のコールバックに渡す
         self.phase += 2 * np.pi * self.frequency * frames / self.samplerate
         self.phase %= 2 * np.pi  # 0から2πの範囲に保つ
+
+        # 最後に生成された音声データを保存
+        self.last_data = np.copy(outdata[:, 0])
 
     def start(self):
         self.stream.start()
--- modules/UI.py
+++ modules/UI.py
@@ -1,15 +1,18 @@
 import tkinter as tk
 from tkinter import ttk
 import math
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 title = "Formant Synth"
 
 
 class UI:
-    def __init__(self, stream, frequency):
+    def __init__(self, stream):
         self.stream = stream
         self.root = tk.Tk()
-        self.frequency = tk.DoubleVar(value=frequency)
+        self.frequency = tk.DoubleVar(value=stream.frequency)
 
         self._buildUI()
         self.root.protocol("WM_DELETE_WINDOW", self._on_destroy)  # windowを閉じるとき
@@ -26,6 +29,7 @@ class UI:
             root,
             from_=100.0,
             to=1000.0,
+            length=500,
             variable=self.frequency,
             orient="horizontal",
             command=self._change_frequency,
@@ -41,19 +45,46 @@ class UI:
             command=self.stream.stop,
         )
 
+        # Matplotlibを埋め込み
+        fig = Fig(root, self.stream)
+        fig.widget.pack()
+
         # レイアウト
         self.label.pack()
         slider.pack()
         button_play.pack()
         button_stop.pack()
 
+    def _updateFig(self, data):
+        plt.cla()  # 現在描写されているグラフを消去
+        plt.xlim(0, 500)
+        plt.ylim(-1.0, 1.0)
+        plt.plot(self.stream.last_data)  # グラフを生成
+
     def _change_frequency(self, val):
         frequency = self.frequency.get()
         self.label["text"] = math.floor(frequency)
         self.stream.frequency = frequency
-
         # print("val:%4d" % frequency)
 
     def _on_destroy(self):
         self.stream.destroy()
+        self.root.quit()
         self.root.destroy()
+
+
+class Fig:
+    def __init__(self, tkroot, stream):
+        self.stream = stream
+
+        fig = plt.figure()
+        anime = animation.FuncAnimation(fig, self._redraw, interval=100)
+        canvas = FigureCanvasTkAgg(fig, tkroot)
+        canvas.draw()
+        self.widget = canvas.get_tk_widget()
+
+    def _redraw(self, data):
+        plt.cla()  # 現在描写されているグラフを消去
+        plt.xlim(0, 500)
+        plt.ylim(-1.0, 1.0)
+        plt.plot(self.stream.last_data)  # グラフを生成
